b8a9874702a7d7dec10dfa288f4b92ff2710ee19
Qetsia Nkulu <qetsiankulu@gmail.com>
2024-03-21 12:45:41 -0700

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Qetsia Nkulu  on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
